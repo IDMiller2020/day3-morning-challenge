7babf3d6a46c1978ebc469472ce52dc2861430d2
IDMiller2020 <psiauto@gmail.com>
2021-03-24 09:15:46 -0600

    Even or Odd function

---
+++ app.js
@@ -0,0 +1,8 @@
+const evenOrOdd = num => {
+  if (num % 2 === 0) {
+    return 'even'
+  } else {return 'odd'}
+}
+
+console.log(`number is ${evenOrOdd(6)}`)
+console.log(`number is ${evenOrOdd(7)}`)
